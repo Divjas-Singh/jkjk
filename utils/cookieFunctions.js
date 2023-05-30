94979a197bcbc14cd419b7544eac6ee7fc44d718
Divjas-Singh <divjassinghanand@gmail.com>
2023-05-30 13:16:06 +0530

    ADDING DOMAINS

--- utils/cookieFunctions.js
+++ utils/cookieFunctions.js
@@ -47,7 +47,7 @@ export const setCookie = async (
     maxAge: 1000 * 60 * 15,
     sameSite: "None",
     secure: true,
-    domain: "http://localhost:5500/index.html",
+    domain: "http://localhost:5500",
   });
   res.status(statusCode).json({
     success: true,
